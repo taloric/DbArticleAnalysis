76648568d5d93052cc9f268897e2991febfdb08a
taloric <taloricag@gmail.com>
2021-02-13 23:15:37 +0800

    init repository add files

---
+++ Analyzer.py
@@ -0,0 +1,9 @@
+import jieba
+from jieba.analyse import *
+
+class Analyzer(object):
+    '''分析器类，用于文本分析'''
+
+    def __init__(self) -> None:
+        super().__init__()
+    
---
+++ Program.py
@@ -0,0 +1,5 @@
+import configparser
+
+
+if __name__ == '__main__':
+    pass
